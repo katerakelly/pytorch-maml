b7060fa5aa1c9bd3fc9abec078b5195d554780b3
Kate Rakelly <rakelly@ada.ist.berkeley.edu>
2017-08-28 23:00:52 -0700

    add training script update gitignore

---
+++ src/train-maml-omniglot.sh
@@ -0,0 +1,12 @@
+exp='maml-omniglot-5way-1shot-adam-TEST'
+dataset='omniglot'
+num_cls=5
+num_inst=2
+batch=1
+m_batch=32
+num_updates=15000
+num_inner_updates=1
+lr='1e-1'
+meta_lr='1e-3'
+gpu=4
+python maml.py $exp --dataset $dataset --num_cls $num_cls --num_inst $num_inst --batch $batch --m_batch $m_batch --num_updates $num_updates --num_inner_updates $num_inner_updates --lr $lr --meta_lr $meta_lr --gpu $gpu 2>&1 | tee ../logs/$exp
