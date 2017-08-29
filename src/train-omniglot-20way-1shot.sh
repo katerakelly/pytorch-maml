ee486eccf7717efc07e952a1f3291ed93aa0045e
Kate Rakelly <rakelly@ada.ist.berkeley.edu>
2017-08-29 11:54:00 -0700

    add training script for each omniglot exp

--- src/train-omniglot-20way-1shot.sh
+++ src/train-omniglot-20way-1shot.sh
@@ -1,12 +1,12 @@
-exp='maml-omniglot-5way-1shot-adam-TEST'
+exp='maml-omniglot-20way-1shot'
 dataset='omniglot'
-num_cls=5
-num_inst=2
+num_cls=20
+num_inst=1
 batch=1
-m_batch=32
-num_updates=15000
-num_inner_updates=1
+m_batch=16
+num_updates=25000
+num_inner_updates=5
 lr='1e-1'
 meta_lr='1e-3'
-gpu=4
+gpu=0
 python maml.py $exp --dataset $dataset --num_cls $num_cls --num_inst $num_inst --batch $batch --m_batch $m_batch --num_updates $num_updates --num_inner_updates $num_inner_updates --lr $lr --meta_lr $meta_lr --gpu $gpu 2>&1 | tee ../logs/$exp
